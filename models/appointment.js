b4748406e77c8e66268a675efd04a5ec1de913ae
Ashish Sharma <ashishsharma01710171@gmail.com>
2026-01-19 22:48:33 +0530

    Rename Appointment model to lowercase 'appointment'

--- models/appointment.js
+++ models/appointment.js
@@ -84,5 +84,5 @@ const appointmentSchema = new Schema({
     },
 }, { timestamps: true });
 
-const Appointment = mongoose.model("Appointment", appointmentSchema);
-module.exports = Appointment;
+const Appointment = mongoose.model("appointment", appointmentSchema);
+module.exports = appointment;
